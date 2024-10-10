48448cbb45da5f1fbc76d78bca17b38176731548
Beltsasar <134458936+Beltsasar@users.noreply.github.com>
2024-10-10 19:44:20 -0300

    holis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
-
+import * as L from 'leaflet';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
-}
+
+  constructor() { }
+
+  ngOnInit() {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    // Solucionar problema de iconos
+    delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: 'assets/marker-icon-2x.png',
+      iconUrl: 'assets/marker-icon.png',
+      shadowUrl: 'assets/marker-shadow.png',
+    });
+
+    // Inicializar el mapa
+    const map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+
+    // Añadir un marcador
+    L.marker([51.505, -0.09]).addTo(map)
+      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      .openPopup();
+  }}
---
+++ src/app/mapa/mapa.component.ts
@@ -0,0 +1,24 @@
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-mapa',
+  templateUrl: './mapa.component.html',
+  styleUrls: ['./mapa.component.scss'],
+})
+export class MapaComponent implements AfterViewInit {
+  private map: any;
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map').setView([51.505, -0.09], 13); // Inicializa el mapa en ngAfterViewInit
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors',
+    }).addTo(this.map);
+  }
+}
